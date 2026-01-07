10800c0766fde8ec6488e5615deabc92973d8929
Nadira Seitkazy <nadiraseitkazu@gmail.com>
2026-01-08 04:13:37 +0500

    Initial Commit

---
+++ IELTSWriting/IELTSWritingApp.swift
@@ -0,0 +1,17 @@
+//
+//  IELTSWritingApp.swift
+//  IELTSWriting
+//
+//  Created by Nadira Seitkazy  on 08.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct IELTSWritingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IELTSWritingTests/IELTSWritingTests.swift
@@ -0,0 +1,17 @@
+//
+//  IELTSWritingTests.swift
+//  IELTSWritingTests
+//
+//  Created by Nadira Seitkazy  on 08.01.2026.
+//
+
+import Testing
+@testable import IELTSWriting
+
+struct IELTSWritingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
